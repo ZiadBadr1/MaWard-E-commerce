9f5ffd44d63dfaacfa1ee68cd63f61fff944bb95
ZiadBadr1 <103049045+ZiadBadr1@users.noreply.github.com>
2024-06-25 01:08:54 +0300

    Update Product Resource Collection

--- app/Http/Resources/ProductResourceCollection.php
+++ app/Http/Resources/ProductResourceCollection.php
@@ -14,20 +14,20 @@ class ProductResourceCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return [
-            'data' => $this->collection,
-            'links' => [
+        return array_merge(
+            [
+                'collection' => $this->collection->toArray(),
+            ],
+            [
                 'next_page_url' => $this->nextPageUrl(),
                 'prev_page_url' => $this->previousPageUrl(),
                 'last' => $this->lastPage(),
-            ],
-            'meta' => [
                 'total' => $this->total(),
                 'per_page' => $this->perPage(),
                 'current_page' => $this->currentPage(),
                 'from' => $this->firstItem(),
                 'to' => $this->lastItem(),
-            ],
-        ];
+            ]
+        );
     }
 }
